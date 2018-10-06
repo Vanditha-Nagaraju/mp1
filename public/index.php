eddbd0fde7c605e32e6e1047bf6bb0a9b0068f33
vanditha <vn245@njit.edu>
2018-10-06 16:18:04 -0400

    Task : Read and tested the csv file data

--- public/index.php
+++ public/index.php
@@ -10,13 +10,42 @@ main::initiate('members.csv');
 
 class main {
 
-    static public function initiate($members){
-        echo $members;
-    }
+            static public function initiate($members){
+
+            $listfile = readFile::getMembersList($members);
+
+        }
 
 }
 
+class readFile {
 
+    public static function getMembersList($members){
+
+        $memberslistFile = fopen($members,"r");
+
+        $listHeader = array();
+
+        $count = 0;
+
+        while(! feof($memberslistFile))
+        {
+
+            $lists = fgetcsv($memberslistFile);
+            if($count == 0) {
+                $listHeader = $lists;
+                echo $listHeader[0];
+            }
+            $count++;
+
+        }
+
+        fclose($memberslistFile);
+        return $lists;
+
+    }
+
+}
 
 
 

07b54f5542327f3e4f7105b72225bc5e646523ec
vanditha <vn245@njit.edu>
2018-10-06 23:25:17 -0400

    Fix : Code edited in Bootstrappage class

--- public/index.php
+++ public/index.php
@@ -99,11 +99,25 @@ class bootstrapPage
         $view = '<!DOCTYPE html><html><head>
           <meta name="viewport" content="width=device-width, initial-scale=1">
           <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        </head><body><div class="container">';
+                  </head><body><div class="container">';
         $view .= $table;
         $view .= '</div></body></html>';
         echo $view;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
